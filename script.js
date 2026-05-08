5a11e27ecabd7f3f6b50f54931b2ff7924f25843
aanmol18 <anmoly683@gmail.com>
2026-05-08 20:19:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const inputs = document.querySelectorAll('.code');
-
+inputs[0].focus();
 inputs.forEach((input, i)=>{
 	input.addEventListener('keyup', (e)=>{
 		if(e.key>='0' && e.key<='9'){
